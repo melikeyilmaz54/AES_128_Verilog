bc029077e49b19b4e4e13a091ebad7c869619809
melikeyilmaz54 <melikemylmz@gmail.com>
2025-08-18 12:36:08 +0300

    Create subBytes.v

---
+++ subBytes.v
@@ -0,0 +1,15 @@
+//128 bitlik giriş değerini sboxdan geçirir.
+module subBytes(//4 parça
+    input [127:0] in,
+    output [127:0] out
+    );
+    genvar i;
+    generate
+    for(i=0; i<128; i=i+8)begin:sub_Bytes //adlandırma
+        sbox s (
+                .in  (in [i+7 : i]),
+                .out (out[i+7 : i])
+            );
+        end    
+    endgenerate
+endmodule
