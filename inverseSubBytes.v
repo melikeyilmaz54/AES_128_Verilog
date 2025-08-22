f40f66ad1ca2a260d4c5780d05c502e439c91c6c
melikeyilmaz54 <melikemylmz@gmail.com>
2025-08-22 12:56:57 +0300

    Update inverseSubBytes.v

--- inverseSubBytes.v
+++ inverseSubBytes.v
@@ -8,8 +8,8 @@ module inverseSubBytes(
             for (i = 0; i < 128; i = i + 8) begin : sub_Bytes
             // Positional yerine named bağlantı daha güvenli:
             inverseSbox s (
-                .in  (in [i+7 : i]),
-                .out (out[i+7 : i])
+                .isbin  (in [i+7 : i]),
+                .isbout (out[i+7 : i])
             );
             end          //[i+7 : i]
     endgenerate
