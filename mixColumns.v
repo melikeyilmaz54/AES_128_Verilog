45a58df60de0a9af8863146bdbbcfecd99259ecb
melikeyilmaz54 <melikemylmz@gmail.com>
2025-08-22 12:58:24 +0300

    Update mixColumns.v

--- mixColumns.v
+++ mixColumns.v
@@ -1,5 +1,5 @@
 //galois cisimciği üzerinde matris çarpımı yapan modül.
-module mixColumns(
+module mixColumns(//ÇALIŞIYOR
     input [127:0] mcin,
     output [127:0] mcout
     );
@@ -21,29 +21,29 @@ module mixColumns(
 			     gm3 = gm2(x) ^ x;
 	       end      
     endfunction
-        //col0 matris çarpımı
-        assign mcout[7:0]   = gm2(mcin[7:0]) ^ gm3(mcin[15:8]) ^ mcin[23:16]      ^ mcin[31:24];
-        assign mcout[15:8]  = mcin[7:0]     ^ gm2(mcin[15:8])   ^ gm3(mcin[23:16]) ^ mcin[31:24];
-        assign mcout[23:16] = mcin[7:0]     ^ mcin[15:8]       ^ gm2(mcin[23:16]) ^ gm3(mcin[31:24]);
-        assign mcout[31:24] = gm3(mcin[7:0]) ^ mcin[15:8]     ^ mcin[23:16]      ^ gm2(mcin[31:24]);
-        
-        //col1 matris çarpımı
-        assign mcout[39:32] = gm2(mcin[39:32]) ^ gm3(mcin[47:40]) ^ mcin[55:48]   ^ mcin[63:56];
-        assign mcout[47:40] = mcin[39:32]    ^ gm2(mcin[47:40])   ^ gm3(mcin[55:48]) ^ mcin[63:56];
-        assign mcout[55:48] = mcin[39:32]    ^ mcin[47:40]        ^ gm2(mcin[55:48]) ^ gm3(mcin[63:56]);
-        assign mcout[63:56] = gm3(mcin[39:32]) ^ mcin[47:40]      ^ mcin[55:48]   ^ gm2(mcin[63:56]);
-        
-        //col3 matris çarpımı
-        assign mcout[71:64] = gm2(mcin[71:64]) ^ gm3(mcin[79:72]) ^ mcin[87:80]     ^ mcin[95:88];
-        assign mcout[79:72] = mcin[71:64]      ^ gm2(mcin[79:72]) ^ gm3(mcin[87:80]) ^ mcin[95:88];
-        assign mcout[87:80] = mcin[71:64]      ^ mcin[79:72]      ^ gm2(mcin[87:80]) ^ gm3(mcin[95:88]);
-        assign mcout[95:88] = gm3(mcin[71:64]) ^ mcin[79:72]      ^ mcin[87:80]     ^ gm2(mcin[95:88]);
-        
-        //col4 matris çarpımı
-        assign mcout[103:96]  = gm2(mcin[103:96]) ^ gm3(mcin[111:104]) ^ mcin[119:112]     ^ mcin[127:120];
-        assign mcout[111:104] = mcin[103:96]      ^ gm2(mcin[111:104]) ^ gm3(mcin[119:112]) ^ mcin[127:120];
-        assign mcout[119:112] = mcin[103:96]      ^ mcin[111:104]      ^ gm2(mcin[119:112]) ^ gm3(mcin[127:120]);
-        assign mcout[127:120] = gm3(mcin[103:96]) ^ mcin[111:104]      ^ mcin[119:112]     ^ gm2(mcin[127:120]);
+           // ---- col0 : [127:120]=r0, [119:112]=r1, [111:104]=r2, [103:96]=r3
+        assign mcout[127:120] = gm2(mcin[127:120]) ^ gm3(mcin[119:112]) ^      mcin[111:104] ^      mcin[103:96];
+        assign mcout[119:112] =      mcin[127:120]  ^ gm2(mcin[119:112]) ^ gm3(mcin[111:104]) ^      mcin[103:96];
+        assign mcout[111:104] =      mcin[127:120]  ^      mcin[119:112] ^ gm2(mcin[111:104]) ^ gm3(mcin[103:96]);
+        assign mcout[103:96]  = gm3(mcin[127:120]) ^      mcin[119:112] ^      mcin[111:104] ^ gm2(mcin[103:96]);
+
+        // ---- col1 : [95:88], [87:80], [79:72], [71:64]
+        assign mcout[ 95: 88] = gm2(mcin[ 95: 88]) ^ gm3(mcin[ 87: 80]) ^      mcin[ 79: 72] ^      mcin[ 71: 64];
+        assign mcout[ 87: 80] =      mcin[ 95: 88]  ^ gm2(mcin[ 87: 80]) ^ gm3(mcin[ 79: 72]) ^      mcin[ 71: 64];
+        assign mcout[ 79: 72] =      mcin[ 95: 88]  ^      mcin[ 87: 80] ^ gm2(mcin[ 79: 72]) ^ gm3(mcin[ 71: 64]);
+        assign mcout[ 71: 64] = gm3(mcin[ 95: 88]) ^      mcin[ 87: 80] ^      mcin[ 79: 72] ^ gm2(mcin[ 71: 64]);
+
+        // ---- col2 : [63:56], [55:48], [47:40], [39:32]
+        assign mcout[ 63: 56] = gm2(mcin[ 63: 56]) ^ gm3(mcin[ 55: 48]) ^      mcin[ 47: 40] ^      mcin[ 39: 32];
+        assign mcout[ 55: 48] =      mcin[ 63: 56]  ^ gm2(mcin[ 55: 48]) ^ gm3(mcin[ 47: 40]) ^      mcin[ 39: 32];
+        assign mcout[ 47: 40] =      mcin[ 63: 56]  ^      mcin[ 55: 48] ^ gm2(mcin[ 47: 40]) ^ gm3(mcin[ 39: 32]);
+        assign mcout[ 39: 32] = gm3(mcin[ 63: 56]) ^      mcin[ 55: 48] ^      mcin[ 47: 40] ^ gm2(mcin[ 39: 32]);
+
+        // ---- col3 : [31:24], [23:16], [15:8], [7:0]
+        assign mcout[ 31: 24] = gm2(mcin[ 31: 24]) ^ gm3(mcin[ 23: 16]) ^      mcin[ 15:  8] ^      mcin[  7:  0];
+        assign mcout[ 23: 16] =      mcin[ 31: 24]  ^ gm2(mcin[ 23: 16]) ^ gm3(mcin[ 15:  8]) ^      mcin[  7:  0];
+        assign mcout[ 15:  8] =      mcin[ 31: 24]  ^      mcin[ 23: 16] ^ gm2(mcin[ 15:  8]) ^ gm3(mcin[  7:  0]);
+        assign mcout[  7:  0] = gm3(mcin[ 31: 24]) ^      mcin[ 23: 16] ^      mcin[ 15:  8] ^ gm2(mcin[  7:  0]);
 
         
 endmodule
